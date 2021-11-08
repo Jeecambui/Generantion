ee0a1b163433f44e03a790c586f83f6abc5fd263
Marcela <almeidamarcelah@gmail.com>
2021-11-08 17:29:52 -0300

    Exercícios em Java

---
+++ Modulo 1/Java/ExerciciosJava1/src/Exercicios/CustoFabrica.java
@@ -0,0 +1,17 @@
+package Exercicios;
+import java.util.Scanner;
+public class CustoFabrica {
+	public static void main(String[] args) {
+		
+		Scanner ler = new Scanner (System.in);
+		System.out.println("Escreva o valor do custo de fábrica");
+		double custoFabrica = ler.nextDouble();
+		double imposto = 0.45;
+		double porcentagemDistribuidor = 0.28;
+		double preco = custoFabrica + custoFabrica * imposto + custoFabrica * porcentagemDistribuidor;
+		System.out.println("O carro custará "+preco);
+		
+				
+	}
+
+}
